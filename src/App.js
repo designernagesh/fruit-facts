223c502f133813d0c255c0b09be087a864c46d4d
designernagesh <desginer.nagesh@gmail.com>
2023-06-01 22:01:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const fruits = [{name: 'Banana', fact: 'I am yellow in color' },
+            { name: 'Peach', fact: 'I have a fuzzy skin' },
+            { name: 'Apple', fact: 'I tend to be crunchy' }];
+  const [ showFruits, setShowFruits ] = useState(false);
+  const btnMessage = 'Show Me Fruit Facts';
+  const btnShowMessage = 'Fruit Facts are Boring';
+
+  const factHandler = () => {
+    setShowFruits(!showFruits);
+    // setButtonMessage('Fruit Facts are Boring');
+  }
+  const fruitsList = <ul>
+  {fruits.map( (fruit, i) => {
+    return (
+      <li key={i}> 
+      <h5>{ fruit.name }</h5>
+      <p>{ fruit.fact }</p>
+      </li>
+    )
+  })}
+</ul>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Fruit Facts</h1>
+      <button onClick={ factHandler }> { showFruits ? btnShowMessage  : btnMessage } </button>
+      { showFruits ? fruitsList : "Click the Button" }
+    </>
   );
 }
 
